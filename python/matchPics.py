1fda6a0321810cc6ab3a8ea5fc79cf679f7e18ea
Ricky Yuan <rickyyuan07@gmail.com>
2024-09-14 16:01:50 -0400

    Finish matching

--- python/matchPics.py
+++ python/matchPics.py
@@ -24,18 +24,19 @@ def matchPics(I1, I2, opts):
         
         ratio = opts.ratio  #'ratio for BRIEF feature descriptor'
         sigma = opts.sigma  #'threshold for corner detection using FAST feature detector'
-        
+                
+        # Convert images to grayscale
+        I1_gray = skimage.color.rgb2gray(I1)
+        I2_gray = skimage.color.rgb2gray(I2)
 
-        # TODO: Convert Images to GrayScale
+        # Detect features in both images using the FAST detector
+        locs1 = corner_detection(I1_gray, sigma)
+        locs2 = corner_detection(I2_gray, sigma)
         
-        
-        # TODO: Detect Features in Both Images
-        
-        
-        # TODO: Obtain descriptors for the computed feature locations
-        
+        # Obtain BRIEF descriptors for the computed feature locations
+        desc1, locs1 = computeBrief(I1_gray, locs1)
+        desc2, locs2 = computeBrief(I2_gray, locs2)
 
-        # TODO: Match features using the descriptors
-        
-
+        # Match features using the BRIEF descriptors
+        matches = briefMatch(desc1, desc2, ratio)
         return matches, locs1, locs2
