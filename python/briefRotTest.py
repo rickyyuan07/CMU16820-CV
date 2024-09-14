f642829dfbfad3c3e651c0eebe90574884bc91b3
Ricky Yuan <rickyyuan07@gmail.com>
2024-09-14 16:02:03 -0400

    Finish brief rotate test

--- python/briefRotTest.py
+++ python/briefRotTest.py
@@ -1,26 +1,42 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+from scipy.ndimage import rotate
 from matchPics import matchPics
 from opts import get_opts
+from tqdm import tqdm
 
 #Q2.1.6
 
 def rotTest(opts):
 
-    # TODO: Read the image and convert to grayscale, if necessary
-
-    for i in range(36):
-
-        # TODO: Rotate Image
-
-        # TODO: Compute features, descriptors and Match features
+    # Read the image and convert to grayscale
+    image = cv2.imread('../data/cv_cover.jpg')
     
-        # TODO: Update histogram
+    # Initialize histogram
+    match_counts = np.zeros(36)
 
-    pass 
+    for angle in tqdm(range(0, 360, 10)):
+        # Rotate Image, reshape=False because it's easier to match features(?)
+        rotated_image = rotate(image, angle=angle, reshape=False)
+
+        # Compute features, descriptors and Match features
+        matches, locs1, locs2 = matchPics(image, rotated_image, opts)
+
+        # Update histogram
+        # (Assuming you have a histogram variable to update)
+        # histogram[i] = len(matches)
+        match_counts[angle//10] = len(matches)
 
 
-    # TODO: Display histogram
+    # Display histogram
+    rotations = np.arange(0, 360, 10)
+    plt.figure()
+    plt.bar(rotations, match_counts, width=8)
+    plt.xlabel('Rotation (degrees)')
+    plt.ylabel('Number of Matches')
+    plt.title('BRIEF Descriptor: Number of Matches vs. Rotation')
+    plt.show()
 
 
 if __name__ == "__main__":
