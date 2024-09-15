3eb27f568cd067afd5ecd9e7702d05392acaf8f9
Ricky Yuan <rickyyuan07@gmail.com>
2024-09-14 23:51:43 -0400

    Generalize the argument of warpImage

--- python/HarryPotterize.py
+++ python/HarryPotterize.py
@@ -11,34 +11,31 @@ from displayMatch import displayMatched
 
 # Q2.2.4
 
-def warpImage(opts):
+def warpImage(opts, template_img, from_img, to_img):
     '''
-    Function to warp 'hp_cover.jpg' onto 'cv_desk.png' using homography
-    computed between from 'cv_cover.jpg' and 'cv_desk.png'.
+    Function to warp from_img(hp_cover.jpg) onto to_img(cv_desk.png) using homography
+    computed between from template image (cv_cover.jpg) and to_img(cv_desk.png).
     '''
-    cv_cover = cv2.imread('../data/cv_cover.jpg')
-    cv_desk = cv2.imread('../data/cv_desk.png')
-    hp_cover = cv2.imread('../data/hp_cover.jpg')
 
-    # Resize hp_cover such that the warpped image fit the correct size as cv_cover
-    hp_cover = cv2.resize(hp_cover, (cv_cover.shape[1], cv_cover.shape[0]))
+    # Resize from_img such that the warpped image fit the correct size as cv_cover
+    from_img = cv2.resize(from_img, (template_img.shape[1], template_img.shape[0]))
 
-    matches, locs1, locs2 = matchPics(cv_desk, cv_cover, opts)
+    matches, locs1, locs2 = matchPics(to_img, template_img, opts)
     locs1 = locs1[matches[:, 0]]
     locs2 = locs2[matches[:, 1]]
     # Important! Swap x, y axis because cv2.warpPerspective treats x, y differently
     locs1 = locs1[:, ::-1]
     locs2 = locs2[:, ::-1]
     
-    # Homography maps locs2 (cv_cover) to locs1 (cv_desk)
+    # Homography maps locs2 (template_img) to locs1 (to_img)
     bestH2to1, _ = computeH_ransac(locs1, locs2, opts)
     
-    composite_img = compositeH(bestH2to1, hp_cover, cv_desk)
+    composite_img = compositeH(bestH2to1, from_img, to_img)
 
     if opts.show_warpped_img:
         cv2.imshow("Composite Image", composite_img)
         cv2.waitKey()
-        
+
     if opts.store_Q225_img:
         cv2.imwrite(f"../Figures/composite_{opts.max_iters}_{opts.inlier_tol}.png", composite_img)
 
@@ -48,6 +45,10 @@ def warpImage(opts):
 if __name__ == "__main__":
 
     opts = get_opts()
-    warpImage(opts)
+
+    cv_cover = cv2.imread('../data/cv_cover.jpg')
+    cv_desk = cv2.imread('../data/cv_desk.png')
+    hp_cover = cv2.imread('../data/hp_cover.jpg')
+    warpImage(opts, cv_cover, hp_cover, cv_desk)
 
 
