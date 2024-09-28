0df6e74a111e6391f637261e98efa41b580340f9
Ricky Yuan <rickyyuan07@gmail.com>
2024-09-28 15:10:09 -0400

    Finish q1.4

--- code/testCarSequenceWithTemplateCorrection.py
+++ code/testCarSequenceWithTemplateCorrection.py
@@ -2,8 +2,8 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-
-# write your script here, we recommend the above libraries for making your animation
+from LucasKanade import LucasKanade
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -22,9 +22,47 @@ parser.add_argument(
     help='threshold for determining whether to update template',
 )
 args = parser.parse_args()
-num_iters = args.num_iters
+num_iters = int(args.num_iters)
 threshold = args.threshold
 template_threshold = args.template_threshold
 
-seq = np.load("../data/carseq.npy")
+seq = np.load("../data/carseq.npy") # shape: (image_height, image_width, num_frames)
 rect = [59, 116, 145, 151]
+rects = []
+
+# The reference template we maintain
+T_frame = seq[:, :, 0]
+T_rect = rect.copy()
+
+# Keep the first template for drift correction
+T1 = seq[:, :, 0]
+T1_rect = rect.copy()
+p_agg = np.zeros(2) # The transformation aggregate from the first frame
+
+for i in tqdm(range(seq.shape[2] - 1)):
+    It = seq[:, :, i] # current
+    It1 = seq[:, :, i + 1] # next
+
+    # Step 1: Perform normal Lucas-Kanade tracking with the current template
+    p = LucasKanade(T_frame, It1, T_rect, threshold, num_iters)
+    pn = p_agg + p
+
+    # Step 2: Perform drift correction with the first template
+    p_star = LucasKanade(T1, It1, T1_rect, threshold, num_iters, p0=p_agg)
+    
+    # If the drift is small, update the template
+    if np.linalg.norm(p_star - pn) <= template_threshold:
+        # Update the template
+        T_frame = It1.copy()
+        T_rect[0] = T1_rect[0] + p_star[0] # x1
+        T_rect[1] = T1_rect[1] + p_star[1] # y1
+        T_rect[2] = T1_rect[2] + p_star[0] # x2
+        T_rect[3] = T1_rect[3] + p_star[1] # y2
+
+        p_agg = p_star
+        # p_agg = pn
+
+    rects.append(T_rect.copy())
+
+# (num_frames, 4)
+np.save("carseqrects-wcrt.npy", rects)
--- code/testGirlSequenceWithTemplateCorrection.py
+++ code/testGirlSequenceWithTemplateCorrection.py
@@ -2,6 +2,8 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from LucasKanade import LucasKanade
+from tqdm import tqdm
 
 # write your script here, we recommend the above libraries for making your animation
 
@@ -22,9 +24,47 @@ parser.add_argument(
     help='threshold for determining whether to update template',
 )
 args = parser.parse_args()
-num_iters = args.num_iters
+num_iters = int(args.num_iters)
 threshold = args.threshold
 template_threshold = args.template_threshold
 
 seq = np.load("../data/girlseq.npy")
 rect = [280, 152, 330, 318]
+rects = []
+
+# The reference template we maintain
+T_frame = seq[:, :, 0]
+T_rect = rect.copy()
+
+# Keep the first template for drift correction
+T1 = seq[:, :, 0]
+T1_rect = rect.copy()
+p_agg = np.zeros(2) # The transformation aggregate from the first frame
+
+for i in tqdm(range(seq.shape[2] - 1)):
+    It = seq[:, :, i] # current
+    It1 = seq[:, :, i + 1] # next
+
+    # Step 1: Perform normal Lucas-Kanade tracking with the current template
+    p = LucasKanade(T_frame, It1, T_rect, threshold, num_iters)
+    pn = p_agg + p
+
+    # Step 2: Perform drift correction with the first template
+    p_star = LucasKanade(T1, It1, T1_rect, threshold, num_iters, p0=p_agg)
+    
+    # If the drift is small, update the template
+    if np.linalg.norm(p_star - pn) <= template_threshold:
+        # Update the template
+        T_frame = It1.copy()
+        T_rect[0] = T1_rect[0] + p_star[0] # x1
+        T_rect[1] = T1_rect[1] + p_star[1] # y1
+        T_rect[2] = T1_rect[2] + p_star[0] # x2
+        T_rect[3] = T1_rect[3] + p_star[1] # y2
+
+        p_agg = p_star
+        # p_agg = pn
+
+    rects.append(T_rect.copy())
+
+# (num_frames, 4)
+np.save("girlseqrects-wcrt.npy", rects)
