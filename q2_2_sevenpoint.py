88bb62a51ec6a18b648f59938d884859e356957a
Ricky Yuan <rickyyuan07@gmail.com>
2024-10-22 21:48:54 -0400

    Finish sevenpoint extra credit

--- q2_2_sevenpoint.py
+++ q2_2_sevenpoint.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from helper import displayEpipolarF, calc_epi_error, toHomogenous, _singularize
+from helper import displayEpipolarF, calc_epi_error, toHomogenous, _singularize, refineF
 
 # Insert your package here
+from tqdm import tqdm
 
 
 """
@@ -26,10 +27,38 @@ Q2.2: Seven Point Algorithm for calculating the fundamental matrix
 
 
 def sevenpoint(pts1, pts2, M):
-    Farray = []
-    # ----- TODO -----
-    # YOUR CODE HERE
-    raise NotImplementedError()
+    T = np.array([[1 / M, 0, 0], [0, 1 / M, 0], [0, 0, 1]])
+    pts1, pts2 = pts1 / M, pts2 / M # Normalizing the points
+    x1, y1 = pts1[:, 0], pts1[:, 1]
+    x2, y2 = pts2[:, 0], pts2[:, 1]
+
+    A = np.array([x2 * x1, x2 * y1, x2, y2 * x1, y2 * y1, y2, x1, y1, np.ones_like(x1)]).T # (N, 9)
+
+    _, _, V = np.linalg.svd(A)
+    F1 = V[-1].reshape(3, 3)
+    F2 = V[-2].reshape(3, 3)
+    
+    # Construct the polynomial for solving alpha
+    f = lambda alpha: np.linalg.det(alpha * F1 + (1 - alpha) * F2)
+    coeff = [0] * 4
+    coeff[0] = (f(2) - f(-2) - 2 * (f(1) - f(-1))) / 12
+    coeff[1] = (f(1) + f(-1)) / 2 - f(0)
+    coeff[2] = (f(1) - f(-1) - 2 * coeff[0]) / 2
+    coeff[3] = f(0)
+    
+    # Might have 1 or 3 real roots
+    roots = np.roots(coeff)
+    roots = [root for root in roots if np.isreal(root)]
+
+    # Construct back to F
+    Farray = [a * F1 + (1 - a) * F2 for a in roots]
+    # Singularize and refine
+    Farray = [_singularize(F) for F in Farray]
+    Farray = [refineF(F, pts1, pts2) for F in Farray]
+    # Unscale, and force F[2, 2] = 1
+    Farray = [T.T @ F @ T for F in Farray]
+    Farray = [F / F[2, 2] for F in Farray]
+
     return Farray
 
 
@@ -57,7 +86,6 @@ if __name__ == "__main__":
 
     # fundamental matrix must have rank 2!
     # assert(np.linalg.matrix_rank(F) == 2)
-    displayEpipolarF(im1, im2, F)
 
     # Simple Tests to verify your implementation:
     # Test out the seven-point algorithm by randomly sampling 7 points and finding the best solution.
@@ -73,7 +101,7 @@ if __name__ == "__main__":
     F_res = []
     choices = []
     M = np.max([*im1.shape, *im2.shape])
-    for i in range(max_iter):
+    for i in tqdm(range(max_iter)):
         choice = np.random.choice(range(pts1.shape[0]), 7)
         pts1_choice = pts1[choice, :]
         pts2_choice = pts2[choice, :]
@@ -87,7 +115,9 @@ if __name__ == "__main__":
     min_idx = np.argmin(np.abs(np.array(ress)))
     F = F_res[min_idx]
     print("Error:", ress[min_idx])
+    print(F)
 
+    displayEpipolarF(im1, im2, F)
     assert F.shape == (3, 3)
     assert F[2, 2] == 1
     assert np.linalg.matrix_rank(F) == 2
